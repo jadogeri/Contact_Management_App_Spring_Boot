8559619dd21993cc6171d4cf326dc67e3ca8f2f8
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-10-05 10:33:12 -0500

    first commit

---
+++ contact-management-app/src/main/java/com/josephadogeri/contact_management_app/ContactManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.josephadogeri.contact_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagementAppApplication.class, args);
+	}
+
+}
